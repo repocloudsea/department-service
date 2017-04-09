9d72025570b041a9ce70eca2d65ace3786256161
Shahbaz <jocnud@gmail.com>
2017-04-09 21:39:25 +0530

    add istall.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+openssl aes-256-cbc -K $encrypted_7a321610dde2_key -iv $encrypted_7a321610dde2_iv -in id_rsa.enc -out id_rsa -d
+chmod 600 id_rsa
+mv deploy-key ~/.ssh/id_rsa
