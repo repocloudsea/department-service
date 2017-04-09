6ef4f256111817e8bb827b09679c2e1e384b47bb
Shahbaz <jocnud@gmail.com>
2017-04-09 22:31:43 +0530

    add istall.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
   
-sftp root@139.59.33.41:target/*.jar /home/cls-repo
+sshpass -p voda*141# sftp root@139.59.33.41:target/*.jar /home/cls-repo
