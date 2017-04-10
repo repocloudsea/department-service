9043e7ab190ca6aefe31ff48fbb194474a87e143
Shahbaz <jocnud@gmail.com>
2017-04-10 16:54:38 +0530

    added rsync

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-sftp -o StrictHostKeyChecking=no sshpass -p -voda*141# root@139.59.33.41:target/department-service-0.0.1-SNAPSHOT.jar /home/cls-repo/department-service-0.0.1-SNAPSHOT.jar
+sftp -o StrictHostKeyChecking=no sshpass -p -voda*141# root@139.59.33.41:target/*.jar /home/cls-repo/
 ##rsync -e ssh file root@139.59.33.41:/home/cls-repo -StrictHostKeyChecking=no
