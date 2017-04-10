9f31f93828614686b1a8b2f1e1ba101df5cf48d9
Shahbaz <jocnud@gmail.com>
2017-04-10 17:43:29 +0530

    added sshpass

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-sshpass -p voda*141# sftp root@139.59.33.41:home/travis/build/repocloudsea/department-service/target/*.jar /home/cls-repo/ -o StrictHostKeyChecking=no
-
+shpass -p voda*141# -o StrictHostKeyChecking=no sftp root@139.59.33.41:home/travis/build/repocloudsea/department-service/target/*.jar /home/cls-repo/
 ##rsync -e ssh file root@139.59.33.41:/home/cls-repo -StrictHostKeyChecking=no
