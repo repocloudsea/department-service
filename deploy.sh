f2d3954fe0d8ab8ed4958b7c63ec744037e42fc7
Shahbaz <jocnud@gmail.com>
2017-04-10 16:18:29 +0530

    added rsync

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 #isftp root@139.59.33.41:target/*.jar /home/cls-repo
-rsync -e ssh file root@139.59.33.41:/home/cls-repo StrictHostKeyChecking=no
+rsync -e ssh file root@139.59.33.41:/home/cls-repo -StrictHostKeyChecking=no
