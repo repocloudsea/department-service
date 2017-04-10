40cf0d9053690d59e5fa984543ede02036b0c5bb
Shahbaz <jocnud@gmail.com>
2017-04-10 17:28:07 +0530

    added sshpass

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_7a321610dde2_key -iv $encrypted_7a321610dde2_iv -in id_rsa.enc -out id_rsa -d
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
+sudo apt-get install sshpass
