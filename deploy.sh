3dcc6c2a28f2e467f5eef78049327995b65b2b62
Shahbaz <jocnud@gmail.com>
2017-04-10 17:23:41 +0530

    added rsync

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-sftp -o StrictHostKeyChecking=no sshpass -pvoda*141#  root@139.59.33.41:home/travis/build/repocloudsea/department-service/target/*.jar /home/cls-repo/
+sshpass -p voda*141# sftp root@139.59.33.41:home/travis/build/repocloudsea/department-service/target/*.jar /home/cls-repo/ -o StrictHostKeyChecking=no
+
 ##rsync -e ssh file root@139.59.33.41:/home/cls-repo -StrictHostKeyChecking=no
