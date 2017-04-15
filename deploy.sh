ba0e47860d5b8f9db51cee66af14b2f26af0dfa5
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-15 18:05:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 set -x # Show the output of the following commands (useful for debugging)
 #sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41:home/travis/.m2/repository/com/cloudsea/services/department-service/0.0.1-SNAPSHOT/department-service-0.0.1-SNAPSHOT.jar root
 ##rsync -e ssh file root@139.59.33.41:/home/cls-repo -StrictHostKeyChecking=no
-touch Sftp_testfile.txt
-pwd
-sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41 <<!
-put Sftp_testfile.txt
-pwd
-bye
-!
+echo "Test File from Travis" >> Sftp_testfile.txt
+echo "put Sftp_testfile.txt" >> sftp_command
+echo "pwd" >> sftp_command
+echo "ls" >> sftp_command
+echo "bye" >> sftp_command
+ls Sftp_testfile.txt sftp_command
+sshpass -p voda*141# StrictHostKeyChecking=no -b sftp_command sftp root@139.59.33.41 
 #sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41:/root Sftp_testfile.txt
