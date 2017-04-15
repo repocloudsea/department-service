73c541b63525e7987ae43435e154d7ab31bfd873
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-15 19:37:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,13 @@ echo "bye" >> sftp_command
 ls Sftp_testfile.txt sftp_command
 cat sftp_command
 which sshpass
+cd
+pwd
+ehco "|1|GICxuhbU1TxOsM+uToSFa9BLQs0=|3yuAMbtUuVRlYRRZdYPzw9Qq8GY= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFP7XY1knPTKcVHMGMuQb8U3fGYYitRcrkQ+krBgSlt5dYuL/vgNT+Pa7wSUqVLhHVtd3ztpX5f6NrRw5VYaZrY=
+|1|iwLcHHmdQ7AbToRVeJmfBFvrQMo=|H6zWNKh0OpqmeMuyjpoLQHX60ac= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCpsYuZf42KZUWY1VRhcirltFZJDi3ZAa7UtBfRu7K2pgkeBdhZ1lzoB8+CRKTWsktWq50f/1ToGwxVpaS6bxs4=
+|1|49uA55PTZjWtx3a9ITA6FFg0fa8=|Edf6nnG/uZbUkmGQN0/H1DNlPWI= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLqZcR4jiHqxImtBglHDrkLgRSI9lGPYyYx1/kp+YzxtI4WxG8s8bqqrD+6SYzJUqHTtLB27+A1ui+LenmcBRKU=
+" >> .ssh/known_hosts
+cat .ssh/known_hosts
 /usr/bin/sshpass -p voda*141#  sftp -oBatchMode=no  -b sftp_command root@139.59.33.41 
 
 #sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41:testfile_demo .
