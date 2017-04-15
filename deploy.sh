699adc4bb467ee2216760b5a622f93e0143e000e
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-15 18:58:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ echo "pwd" >> sftp_command
 echo "ls" >> sftp_command
 echo "bye" >> sftp_command
 ls Sftp_testfile.txt sftp_command
+which sshpass
 sshpass -p voda*141# StrictHostKeyChecking=no -oBatchMode=no sftp -b sftp_command root@139.59.33.41 
 #sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41:testfile_demo .
 
