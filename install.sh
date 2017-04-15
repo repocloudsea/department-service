1a1f38c984b108dd53d1a0e3983e696966b2ecd9
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-15 22:07:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ openssl aes-256-cbc -K $encrypted_7a321610dde2_key -iv $encrypted_7a321610dde2_i
 eval "$(ssh-agent -s)"
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
-/usr/bin/sshpass -p 'voda*141#' ssh-add ~/.ssh/id_rsa
+#/usr/bin/sshpass -p 'voda*141#' ssh-add ~/.ssh/id_rsa
 
