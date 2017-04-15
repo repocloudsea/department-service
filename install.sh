88fb0432a35c02466d061b0561931fac80d2fcf0
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-15 21:58:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_7a321610dde2_key -iv $encrypted_7a321610dde2_iv -in id_rsa.enc -out id_rsa -d
+eval "$(ssh-agent -s)"
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
+
