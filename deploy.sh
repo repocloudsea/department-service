9e92e9335c2906a8a709cfade2925e24c358d7c3
Khan <sk052595@m1525250.northamerica.cerner.net>
2017-04-17 18:06:58 +0530

    using scp

--- deploy.sh
+++ deploy.sh
@@ -28,4 +28,4 @@ pwd
 cd target
 pwd
 ls
-sshpass -p voda*141# scp department-service-0.0.1-SNAPSHOT.jar root@139.59.33.41:/home -o stricthostkeychecking=no
+sshpass -p voda*141# scp -o stricthostkeychecking=no department-service-0.0.1-SNAPSHOT.jar root@139.59.33.41:/home
