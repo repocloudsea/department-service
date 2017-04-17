a414771d4f642a21c0c92c51cb2f39e86b730139
parvezbinhahmed <parvezbinahmed@gmail.com>
2017-04-17 23:13:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,6 +27,8 @@ set -x # Show the output of the following commands (useful for debugging)
 pwd
 cd target
 pwd
+cat ~/.ssh/known_hosts
 ls -lrt department-service-0.0.1-SNAPSHOT.jar
 #sshpass -p 'Kol@@21' scp -v -q department-service-0.0.1-SNAPSHOT.jar redhat@139.59.33.41:/home/redhat
-sshpass -p 'Kol@@21' scp -v  department-service-0.0.1-SNAPSHOT.jar redhat@139.59.33.41:/home/redhat
+#sshpass -p 'Kol@@21' scp -v  department-service-0.0.1-SNAPSHOT.jar redhat@139.59.33.41:/home/redhat
+sshpass -p 'Kol@@21' ssh user@server ls -la
