643894f5573102d938be897474197416bdf2eb9b
Khan <sk052595@m1525250.northamerica.cerner.net>
2017-04-17 17:14:48 +0530

    testing with scp

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,24 @@ set -x # Show the output of the following commands (useful for debugging)
 #pwd
 #cd
 #pwd
-echo "Test File from Travis : NEw" >> Sftp_testfile.txt
-echo "put Sftp_testfile.txt" >> sftp_command
-echo "pwd" >> sftp_command
-echo "ls" >> sftp_command
-echo "bye" >> sftp_command
-echo "exit" >> sftp_command
-ls Sftp_testfile.txt sftp_command
-cat sftp_command
-which sshpass
-ls /home/travis/.ssh/id_rsa
-pwd
-/usr/bin/sshpass -p 'voda*141#'  sftp -v -oStrictHostKeyChecking=no -oGSSAPIAuthentication=no -oBatchMode=no  -b - root@139.59.33.41 <<!
-put Sftp_testfile.txt
-pwd
-bye
-!
+#echo "Test File from Travis : NEw" >> Sftp_testfile.txt
+#echo "put Sftp_testfile.txt" >> sftp_command
+#echo "pwd" >> sftp_command
+#echo "ls" >> sftp_command
+#echo "bye" >> sftp_command
+#echo "exit" >> sftp_command
+#ls Sftp_testfile.txt sftp_command
+#cat sftp_command
+#which sshpass
+#ls /home/travis/.ssh/id_rsa
+#pwd
+#/usr/bin/sshpass -p 'voda*141#'  sftp -v -oStrictHostKeyChecking=no -oGSSAPIAuthentication=no -oBatchMode=no  -b - root@139.59.33.41 <<!
+#put Sftp_testfile.txt
+#pwd
+#bye
+#!
 
 #sshpass -p voda*141# StrictHostKeyChecking=no sftp root@139.59.33.41:testfile_demo .
 
+
+sshpass -e scp home/travis/.m2/repository/com/cloudsea/services/department-service/0.0.1-SNAPSHOT/department-service-0.0.1-SNAPSHOT.jar root@$139.59.33.41:/root -o stricthostkeychecking=no
