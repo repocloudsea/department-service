01882c9a4de4cad1eca4a2127f4b412457845c2c
Shahbaz <jocnud@gmail.com>
2017-05-04 21:25:38 +0530

    removed files used for scripted deployment

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x # Show the output of the following commands (useful for debugging)
-openssl aes-256-cbc -K $encrypted_7a321610dde2_key -iv $encrypted_7a321610dde2_iv -in id_rsa.enc -out id_rsa -d
-eval "$(ssh-agent -s)"
-chmod 600 id_rsa
-mv id_rsa ~/.ssh/id_rsa
-#/usr/bin/sshpass -p 'voda*141#' ssh-add ~/.ssh/id_rsa
-
